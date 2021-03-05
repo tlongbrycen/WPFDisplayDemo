eda551020e508d555bff46e8c841575a9718e459
tlong <tlong@brycen.co.jp>
2021-03-05 14:06:50 +0900

    MVVM

---
+++ WPFDisplayDemo/ViewModel/ButtonSetViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WPFDisplayDemo.ViewModel
+{
+    class ButtonSetViewModel
+    {
+        public MyICommand<CheckBox> ClickCommand { get; set; }
+
+        public ButtonSetViewModel()
+        {
+            ClickCommand = new MyICommand<CheckBox>(OnClick);
+        }
+
+        private void OnClick(CheckBox checkBox)
+        {
+            if((bool)checkBox.IsChecked)
+            {
+                Debug.WriteLine("CheckBox is checked");
+            }
+            else
+            {
+                Debug.WriteLine("CheckBox is not checked");
+            }
+        }
+    }
+}
